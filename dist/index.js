7aa7fe9d1044c3b84713d24c3a72b54d67890c50
AJ Butler <ajbutler195@gmail.com>
2023-03-21 01:12:30 -0400

    removed horizontal scrollbar

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,27 @@
 const membersContainer = document.getElementById('membersContainer');
 const membersScrollWidth = membersContainer.scrollWidth;
+const membersWidth = membersContainer.clientWidth;
+let direction;
 
 window.addEventListener('load', () => {
   self.setInterval(() => {
-    if (membersContainer.scrollLeft !== membersScrollWidth) {
+    if (membersContainer.scrollLeft !== membersScrollWidth - membersWidth && direction == 'L') {
       membersContainer.scrollTo(membersContainer.scrollLeft + .5, 0);
     }
-    // else if (membersContainer.scrollRight !== membersScrollWidth) {
-    //   membersContainer.scrollTo(membersContainer.scrollRight + .5, 0);
-    // }
+    else if (membersContainer.scrollLeft == membersScrollWidth - membersWidth) {
+      direction = 'R'
+      membersContainer.scrollTo(membersContainer.scrollLeft - .5, 0);
+    }
+    else if (membersContainer.scrollLeft !== 0 && direction == 'R') {
+      membersContainer.scrollTo(membersContainer.scrollLeft - .5, 0);
+    }
+    else if (membersContainer.scrollLeft == 0) {
+      direction = 'L'
+      membersContainer.scrollTo(membersContainer.scrollLeft + .5, 0);
+    }
+    console.log(direction);
+    console.log(membersContainer.scrollLeft);
+    console.log(membersScrollWidth - membersWidth);
+    console.log(membersScrollWidth);
   }, 15);
 });
