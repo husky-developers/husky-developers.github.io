7113cf7b62a68cb484bd4e421ef40a133196c7ea
AJ Butler <ajbutler195@gmail.com>
2023-03-21 01:17:11 -0400

    auto scroll right, discrepancy (TO FIX)

--- dist/index.js
+++ dist/index.js
@@ -8,18 +8,18 @@ window.addEventListener('load', () => {
     if (membersContainer.scrollLeft !== membersScrollWidth - membersWidth && direction == 'L') {
       membersContainer.scrollTo(membersContainer.scrollLeft + .5, 0);
     }
-    else if (membersContainer.scrollLeft == membersScrollWidth - membersWidth) {
-      direction = 'R'
-      membersContainer.scrollTo(membersContainer.scrollLeft - .5, 0);
-    }
-    else if (membersContainer.scrollLeft !== 0 && direction == 'R') {
-      membersContainer.scrollTo(membersContainer.scrollLeft - .5, 0);
-    }
-    else if (membersContainer.scrollLeft == 0) {
-      direction = 'L'
-      membersContainer.scrollTo(membersContainer.scrollLeft + .5, 0);
-    }
-    // console.log(direction);
+    // else if (membersContainer.scrollLeft == membersScrollWidth - membersWidth) {
+    //   direction = 'R'
+    //   membersContainer.scrollTo(membersContainer.scrollLeft - .5, 0);
+    // }
+    // else if (membersContainer.scrollLeft !== 0 && direction == 'R') {
+    //   membersContainer.scrollTo(membersContainer.scrollLeft - .5, 0);
+    // }
+    // else if (membersContainer.scrollLeft == 0) {
+    //   direction = 'L'
+    //   membersContainer.scrollTo(membersContainer.scrollLeft + .5, 0);
+    // }
+    // // console.log(direction);
     // console.log(membersContainer.scrollLeft);
     // console.log(membersScrollWidth - membersWidth);
     // console.log(membersScrollWidth);
