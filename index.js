a20b7cabea2daa753bdb61d8a079331189f91af2
AJ Butler <ajbutler195@gmail.com>
2023-03-21 00:05:39 -0400

    Updates

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const flavoursContainer = document.getElementById('flavoursContainer');
-const flavoursScrollWidth = flavoursContainer.scrollWidth;
+const membersContainer = document.getElementById('membersContainer');
+const membersScrollWidth = membersContainer.scrollWidth;
 
 window.addEventListener('load', () => {
   self.setInterval(() => {
-    if (flavoursContainer.scrollLeft !== flavoursScrollWidth) {
-      flavoursContainer.scrollTo(flavoursContainer.scrollLeft + 1, 0);
+    if (membersContainer.scrollLeft !== membersScrollWidth) {
+      membersContainer.scrollTo(membersContainer.scrollLeft + .5, 0);
     }
+    // else if (membersContainer.scrollRight !== membersScrollWidth) {
+    //   membersContainer.scrollTo(membersContainer.scrollRight + .5, 0);
+    // }
   }, 15);
 });
